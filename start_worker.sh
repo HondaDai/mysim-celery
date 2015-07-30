006cfd7ada58555232cde0b3b0038335e12e1164
HondaDai <hondadai.tw@gmail.com>
2015-07-30 23:34:13 +0800

    multi worker

--- start_worker.sh
+++ start_worker.sh
@@ -1,1 +1,6 @@
-celery -A tasks worker --loglevel=info --concurrency=1 -- celeryd.prefetch_multiplier=1 
+celery multi start \
+  -A tasks worker \
+  --loglevel=info \
+  --concurrency=1 \
+  --logfile="$HOME/log/celery/%n.log" \
+  -- celeryd.prefetch_multiplier=1 
