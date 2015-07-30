4bfee862e5bf148c71e4972fd2e2ce6a298ea41a
HondaDai <hondadai.tw@gmail.com>
2015-07-30 23:47:32 +0800

    record pid

--- start_worker.sh
+++ start_worker.sh
@@ -2,5 +2,6 @@ celery multi start \
   -A tasks worker \
   --loglevel=info \
   --concurrency=1 \
+  --pidfile="$HOME/log/celery/%n.pid" \
   --logfile="$HOME/log/celery/%n.log" \
   -- celeryd.prefetch_multiplier=1 
