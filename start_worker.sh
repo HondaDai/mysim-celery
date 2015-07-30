ae821f6991986dd92f348b1c1a001141a96289e4
HondaDai <hondadai.tw@gmail.com>
2015-07-31 00:25:41 +0800

    nohup

--- start_worker.sh
+++ start_worker.sh
@@ -1,7 +1,9 @@
-nohup celery \
+celery \
   -A tasks worker \
   --loglevel=info \
   --concurrency=1 \
   --pidfile="$HOME/log/celery/%n.pid" \
   --logfile="$HOME/log/celery/%n.log" \
   -- celeryd.prefetch_multiplier=1 
+
+# usage: nohup sh start_worker.sh
