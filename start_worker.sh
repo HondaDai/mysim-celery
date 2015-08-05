b6560434efc1a33341d4b76e5eb3c396203ff09b
HondaDai <hondadai.tw@gmail.com>
2015-08-06 01:06:43 +0800

    concurrency

--- start_worker.sh
+++ start_worker.sh
@@ -1,7 +1,7 @@
 nohup celery \
   -A tasks worker \
   --loglevel=info \
-  --concurrency=1 \
+  --concurrency=4 \
   --pidfile="$HOME/log/celery/%n.pid" \
   --logfile="$HOME/log/celery/%n.log" \
-  -- celeryd.prefetch_multiplier=1 &
+  -- celeryd.prefetch_multiplier=4 &
